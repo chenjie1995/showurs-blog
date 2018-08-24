457cff27f495b486229e2cdc5f95b747cbdda756
chenjie <chenjie1995@live.com>
2018-08-24 17:22:50 +0800

    init

--- blog-config/src/main/java/cn/showurs/blog/config/BlogConfigApplication.java
+++ blog-config/src/main/java/cn/showurs/blog/config/BlogConfigApplication.java
@@ -3,7 +3,10 @@ package cn.showurs.blog.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class BlogConfigApplication {
 
