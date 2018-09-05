dbd6fe9f00b6794d995dbe88b34c268a279ff84c
chenjie <chenjie1995@live.com>
2018-09-05 17:00:06 +0800

    config & bus

--- blog-article/src/main/java/cn/showurs/blog/article/controller/ArticleController.java
+++ blog-article/src/main/java/cn/showurs/blog/article/controller/ArticleController.java
@@ -3,6 +3,8 @@ package cn.showurs.blog.article.controller;
 import cn.showurs.blog.article.client.UserClient;
 import cn.showurs.blog.article.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by CJ on 2018/9/3 15:35.
  */
+@RefreshScope
 @RestController
 @RequestMapping("articles")
 public class ArticleController {
     private UserClient userClient;
+
+    @Value("${test.name}")
+    private String value;
 
     @Autowired
     public ArticleController(UserClient userClient) {
@@ -23,5 +29,10 @@ public class ArticleController {
     @GetMapping("user")
     public UserVo getArticleUser() {
         return userClient.get(1);
+    }
+
+    @GetMapping("value")
+    public String getValue() {
+        return value;
     }
 }
