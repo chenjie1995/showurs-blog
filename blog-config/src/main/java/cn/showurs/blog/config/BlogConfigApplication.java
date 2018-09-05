268a177c3f78af3835b9bf62ead752339922435d
chenjie <chenjie1995@live.com>
2018-09-05 11:25:52 +0800

    spring cloud config service

--- blog-config/src/main/java/cn/showurs/blog/config/BlogConfigApplication.java
+++ blog-config/src/main/java/cn/showurs/blog/config/BlogConfigApplication.java
@@ -3,6 +3,7 @@ package cn.showurs.blog.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
