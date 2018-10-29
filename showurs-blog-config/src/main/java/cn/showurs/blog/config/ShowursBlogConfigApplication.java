da44e1dcbcd69790e04cb63a24ac47715e930716
chenjie <chenjie1995@live.com>
2018-10-29 17:12:41 +0800

    重构

---
+++ showurs-blog-config/src/main/java/cn/showurs/blog/config/ShowursBlogConfigApplication.java
@@ -0,0 +1,12 @@
+package cn.showurs.blog.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowursBlogConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowursBlogConfigApplication.class, args);
+    }
+}
---
+++ showurs-blog-eureka/src/main/java/cn/showurs/blog/eureka/ShowursBlogEurekaApplication.java
@@ -0,0 +1,12 @@
+package cn.showurs.blog.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowursBlogEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowursBlogEurekaApplication.class, args);
+    }
+}
