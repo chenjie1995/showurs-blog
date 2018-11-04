94174411625e21d54d0b76af7a7f16a5345413ea
chenjie <chenjie1995@live.com>
2018-11-04 22:17:12 +0800

    更改配置,添加微服务

---
+++ showurs-blog-user/src/main/java/cn/showurs/blog/user/ShowursBlogUserApplication.java
@@ -0,0 +1,12 @@
+package cn.showurs.blog.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowursBlogUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowursBlogUserApplication.class, args);
+    }
+}
